00c61e2fa8c5b889eee7a6d6ac73e2460705f419
manhdung99it <manhdung99it@gmail.com>
2024-06-15 02:32:57 +0700

    upload interview practice

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,118 @@
+<template>
+  <div class="header">
+    <div class="header-content">
+      <div class="logo-wrapper">
+        <img :src="logo" alt="" srcset="" />
+      </div>
+      <div class="action-wrapper">
+        <button class="simple-button">Indie Game</button>
+        <button class="simple-button">Entry Game</button>
+        <button class="simple-button">Community</button>
+      </div>
+    </div>
+  </div>
+  <div :style="`font-size:${fontSize}px`" class="main-content">
+    <div class="select-wrapper">
+      <h2>Photo Cards</h2>
+      <div>
+        <span style="font-weight: 700"> Font-size: </span>
+        <select name="fontSize" id="fontSize" v-model="fontSize">
+          <option v-for="number in numberList" :value="number">
+            {{ number }}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="list-image">
+      <card
+        v-for="number in numberList"
+        :key="number"
+        :image-src="number % 2 == 0 ? image1 : image2"
+        description="Product name is quite long in some cases"
+      />
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
+import logo from "./assets/images/logo.png";
+import image1 from "./assets/images/image1.jpg";
+import image2 from "./assets/images/image2.png";
+import card from "./components/elements/card.vue";
+import { computed, ref } from "vue";
 
-<template>
-  <HelloWorld />
-</template>
+const numberList = computed(() => {
+  return Array.from({ length: 28 }, (_, i) => i + 1);
+});
+const fontSize = ref(13);
+</script>
+<style lang="scss">
+body {
+  margin: 0;
+}
+.header {
+  background: #2ccd9b;
+  padding: 0 4px;
+  color: white;
+  overflow: hidden;
+  .header-content {
+    display: flex;
+    padding: 20px 0;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1536px;
+    width: 100%;
+    margin: auto;
+  }
+  .action-wrapper {
+    display: flex;
+    column-gap: 10px;
+    .simple-button {
+      border: 1px solid #fff;
+      background: transparent;
+      color: white;
+      padding: 8px 8px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    :hover {
+      font-weight: 700;
+    }
+  }
+}
+.main-content {
+  max-width: 1536px;
+  width: 100%;
+  margin: auto;
+  .list-image {
+    margin: 0 4px;
+    display: grid;
+    grid-gap: 12px;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  }
+  .select-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 4px;
+  }
+}
+// @media (min-width: 550px) {
+//   .list-image {
+//     grid-template-columns: repeat(2, 1fr);
+//   }
+// }
+
+// @media (min-width: 750px) {
+//   .list-image {
+//     grid-template-columns: repeat(3, 1fr);
+//   }
+// }
+@media (min-width: 1536px) {
+  .list-image {
+    grid-template-columns: repeat(6, 1fr) !important;
+  }
+}
+</style>
---
+++ src/components/elements/card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <img :src="imageSrc" alt="Card image" />
+    </div>
+    <div class="card-content">
+      <p class="card-text">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, nextTick, ref } from "vue";
+const props = defineProps<{
+  imageSrc: string;
+  description: string;
+}>();
+</script>
+<style scoped>
+.card {
+  background: white;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.card-image {
+  width: 100%;
+  padding-top: 66.66%; /* 3:2 aspect ratio */
+  position: relative;
+}
+.card-image img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.card-text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-weight: 700;
+  margin: 0;
+  padding: 4px 4px;
+}
+</style>
